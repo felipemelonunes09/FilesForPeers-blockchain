9f34f6bfc51ccb125dc6847f07c3a223bc4bd2d9
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-12-01 01:32:51 -0300

    add consensus server

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import time
 import logging
 import multiprocessing
 from data.server import Server as DataLayerServer
 from network.server import Server as NetworkLayerServer
-from consensus.Server import Server as ConsensusServer
+from consensus.server import Server as ConsensusServer
 
 def start_data_layer():
     server = DataLayerServer()
@@ -23,7 +24,10 @@ if __name__ == "__main__":
     process_network = multiprocessing.Process(target=start_network_layer)
     process_network.start()
     
+    time.sleep(2)
+    
     process_consensus = multiprocessing.Process(target=start_consesus_layer)
     process_consensus.start()
+
     
         
